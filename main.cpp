2817f32970eea449659c9190640afef21cb09c75
Елисей Ли <elisey.li.2002@mail.ru>
2020-10-10 13:03:09 +1000

    first

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void task1()
+{
+    unsigned int n;
+    cout << "TASK__1" << endl << "n = ";
+    cin >> n;
+    cout << (1 << n) << endl;
+}
+
+int main()
+{
+    task1();
+
+    return 0;
+}
